ffdb80e2e876ed5de357eac9a0d2b0032bd053fb
Asadbek <ibodullaev.asadbek@mail.ru>
2024-12-03 22:59:13 +0500

    fix: full solution

--- day-2/solution.js
+++ day-2/solution.js
@@ -11,69 +11,105 @@ const __dirname = path.dirname(__filename);
 
 
 try {
+    // Read data from input.txt file
     const data = await fs.readFile(path.join(__dirname, 'input.txt'), { encoding: 'utf8' });
-
-
-
+    // Split each line
     const rows = data.trim().split("\n");
+    // Convert data(numbers) into 2D array
     const number2D = rows.map(row => row.split(' ').map(Number))
 
+
+    // Part one
+
+    // Finds Ascending sub arrays in number2D array and returns true or false
     const isAscending = (arr) => {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i - 1]) {
-                return false;
-            } else {
-                return true;
-            }
+        for (let i = 0; i < arr.length - 1; i++) {
+            const diff = arr[i + 1] - arr[i];
+            if (diff < 1 || diff > 3) return false;
+            if (diff <= 0) return false;
 
         }
 
-    }
+        return true;
 
+    }
+    // Finds Descending sub arrays in number2D array and returns true or false
     const isDescending = (arr) => {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[i - 1]) {
-                return false;
-            } else {
-                return true;
-            }
+        for (let i = 0; i < arr.length - 1; i++) {
+            const diff = arr[i] - arr[i + 1];
+            if (diff < 1 || diff > 3) return false;
+            if (diff <= 0) return false;
 
         }
+        return true;
     }
-
+    // Intilize var to count number of sub arrays
     let sortedCount = 0;
-
-
-
+    // Loops number2D arrays and gives each sub array to 2 function
     number2D.forEach(row => {
-
-        console.log('1', isAscending(row));
-
-        if (isAscending(row)) {
+        if (isAscending(row) || isDescending(row)) {
             sortedCount++;
         }
-        // if (isDescending(row)) {
-        //     sortedCount++;
-        // }
+
     });
-
-    console.log(sortedCount);
-
+    console.log(`Part one ans: ${sortedCount}`);
 
 
+    // Part two
+
+    const canBe = (arg) => {
+
+        const isDescending = (arr) => {
+            for (let i = 0; i < arr.length - 1; i++) {
+                const diff = arr[i] - arr[i + 1];
+                if (diff < 1 || diff > 3) return false;
+                if (diff <= 0) return false;
+
+            }
+            return true;
+        }
+
+        const isAscending = (arr) => {
+            for (let i = 0; i < arr.length - 1; i++) {
+                const diff = arr[i + 1] - arr[i];
+                if (diff < 1 || diff > 3) return false;
+                if (diff <= 0) return false;
+
+            }
+
+            return true;
+
+        }
 
 
 
+        // Loops every element in sub array
+        for (let i = 0; i < arg.length; i++) {
+            const modefiedSequence = arg.slice(0, i).concat(arg.slice(i + 1));
+            if (isAscending(modefiedSequence) || isDescending(modefiedSequence)) {
+                return true;
+            }
+
+        }
 
 
+        return false;
 
+    }
 
+    const countModefiedSeq = (data) => {
+        let count = 0;
+        data.forEach(row => {
+            if (canBe(row)) {
+                count++;
+            }
 
+        })
 
+        return count;
+    }
 
-
-
-
+    console.log(`Part two ans: ${countModefiedSeq(number2D)}`);
 
 
 
